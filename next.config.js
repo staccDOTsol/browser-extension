6ea0a8a5129a06064a799fdfbd162c60f683789b
redacted_jare <redacted_jare@penguin>
2022-02-20 22:14:34 -0400

    a

---
+++ next.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: false,
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      os: false,
+    };
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    config.module.rules.push({
+      test: /\.(j|t)(s|sx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ["@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript"]
+      }
+    });
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@solana/wallet-adapter-react": path.resolve(
+        "./node_modules/@solana/wallet-adapter-react"
+      ),
+    };
+    return config;
+  },
+};
